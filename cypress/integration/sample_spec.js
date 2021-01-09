f35b7a036ea3710025e9c998836f0e6c4fe88ed7
chiragmahawar7 <cmahawar_be16@thapar.edu>
2021-01-09 22:12:45 +0530

    cypress e2e testing setup

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,9 @@
+describe('Shopping Cart', () => {
+    it('Sample test', () => {
+        expect(true).to.equal(true);
+    });
+
+    it('visit the page', () => {
+        cy.visit('/');
+    });
+});
